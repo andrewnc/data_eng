d2760f099905fcc73d093903d6e0cb16ac6d7c17
mckaycourt <mckay.court@gmail.com>
2018-10-11 08:50:58 -0600

    Me figuring things out :)

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 import requests
 
 def query_for_machine(host):
+    print(requests.post("http://127.0.0.1:5984/resources/_find/", json={"selector":{"host":{"$eq": host}}}))
     return requests.post("http://127.0.0.1:5984/resources/_find/", json={"selector":{"host":{"$eq": host}}}).text
 
 if __name__ == "__main__":
