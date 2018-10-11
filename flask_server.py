9f3cdac1285ef9d10c575e380d2bcf605ef0a8c7
mckaycourt <mckay.court@gmail.com>
2018-10-11 09:17:13 -0600

    Adding a route to host the javascript file

--- flask_server.py
+++ flask_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from query import query_for_machine
 import scraper
 app = Flask(__name__)
@@ -10,6 +10,11 @@ def hello():
         print(query_for_machine(host))
         hosts.append(query_for_machine(host))
     return render_template("index.html", metrics=hosts)
+
+
+@app.route("/createHighCharts.js")
+def send_js():
+    return send_from_directory('/', 'createHighCharts.js')
 
 
 if __name__ == "__main__":
